94b6603292f715a82254aa8eee0ac3594f9d74c5
GianW <gian.cb@hotmail.com>
2014-10-23 16:32:59 -0200

    Update

--- teste_node.js
+++ teste_node.js
@@ -6,52 +6,28 @@ var palavraSchema = new Mongoose.Schema(
         valores: Object
     });
 
-
-
 var http      = require('http');
 
 function onRequest(request, response){
     console.log("Requisição recebida");
     response.writeHead(200, {"Content-Type": "text;plain"});
 
-     /*if (request.url === "/") {
-        response.write("Sorry Buddy :(");
-    }else{       
-    	var parametroRecebido   = request.url.split("=");
-        var resposta            = parametroRecebido.length.toString();
-                    
-        if (parametroRecebido[0] === "/pesquisa"){
-            resposta = parametroRecebido[1].toString();
-            
-            // var testeFunc = pesquisaPalavra();
-            // console.log(testeFunc);*/
-            var info = "";
-            info = pesquisaPalavra(function(data){
-                // response.write(data);   
-                console.log("callback");            
-                return data;
-
-            });
-            //response.write(testeFunc);
-            console.log("f request");
-
-            if (info != "") {
-                response.write(info);    
-            };
-
-            response.write(request.url + info);  
-            
-            
-        // }
-    // }    
-    response.end();
+    pesquisaPalavra(function(data){
+        
+        if (data != "") {
+          response.write(data);    
+        };
+       
+	   response.end();        
+    });
+          
  };
 
 http.createServer(onRequest).listen(1337, '127.0.0.1');
 console.log("Node.js foi iniciado");
 Mongoose.connect('mongodb://localhost/pesquisa'); 
 
-function pesquisaPalavra(chamada){
+function pesquisaPalavra(callback){
     
     var pesqPalavra = Mongoose.model('palavras', palavraSchema);
     var db          = Mongoose.connection;
@@ -65,22 +41,13 @@ function pesquisaPalavra(chamada){
  
     var arrayPalavras = new Array();
     arrayPalavras.push("nome: dedo");
-    arrayPalavras.push("nome: quebrei");
+    // arrayPalavras.push("nome: quebrei");
 
     // pesqPalavra.findOne({ nome: 'dedo' },'valores', function(err, palRetorno) {   
     pesqPalavra.find( {$or:[arrayPalavras]},'valores', function(err, palRetorno) {   
          if (err) return console.error(err);   
-            retorno_r = JSON.stringify(palRetorno);
-            console.log(retorno_r);
-        // if (chamada && typeof(chamada) == "function") {
-        //     retorno_r = chamada(JSON.stringify(palRetorno));
-        // };         
-         //retorno = JSON.stringify(palRetorno);
+         retorno_r = JSON.stringify(palRetorno);
+         console.log(retorno_r);
+         callback(retorno_r);
     });
-
-   // db.close();                              
-    console.log("primeira func");
-
-	return retorno_r;
-
 }
